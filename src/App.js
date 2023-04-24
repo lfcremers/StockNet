5b4e90d59eafb39d5a6925cfa48948d09300da00
lcremers <lfc2136@columbia.edu>
2023-04-24 08:29:12 -0400

    setup to include stocknet_speeder almost done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 // function App() {
@@ -24,10 +24,122 @@ import './App.css';
 
 // export default App;
 
-import React from "react";
+// import React from "react";
 
-const App = () => {
-  return <div>Hello World</div>;
-};
+import {Space, Table, Tag } from 'antd';
+
+// const App = () => {
+//   return (
+//     <div className = "app-container">
+//     <table>
+//       <thread>
+//         <tr>
+//           <th>A        </th>
+//           <th>12      </th>
+//           <th>2        </th>
+//         </tr>
+//       </thread>
+//       <tbody>
+//         <tr>
+//           <td>B</td>
+//           <td>3</td>
+//           <td>4</td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   </div>
+//   );
+// };
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+  {
+    title: 'Color',
+    key: 'color',
+    render(record, {color}) {
+      return { 
+        props: { 
+          style: {background: (color)}
+        }, (
+         <Space size="middle">
+        <a>Invite {record.name}</a>/
+        <a>Delete</a>
+        </Space> )
+      };
+    }
+  },
+];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+    color: 'red',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+    color: 'yellow',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+    color: 'red',
+  },
+];
+const App = () => <Table columns={columns} dataSource={data} />;
 
 export default App;
